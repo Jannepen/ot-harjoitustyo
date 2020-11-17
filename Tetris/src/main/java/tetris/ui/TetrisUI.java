83e8a338b341a3dd176121cc371430eb0423a4bf
Penttala Janne O <jannepen@cubbli18-gold.local>
2020-11-17 09:22:27 +0200

    alustus

---
+++ Tetris/src/main/java/tetris/ui/TetrisUI.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetris.ui;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jannepen
+ */
+public class TetrisUI extends Application {
+    
+    @Override
+    public void start(Stage window) throws Exception {
+        window.setTitle("Hello world");
+        window.show();
+    }
+    
+//    public static void main(String[] args) {
+//        launch(TetrisUI.class);
+//    }
+}
